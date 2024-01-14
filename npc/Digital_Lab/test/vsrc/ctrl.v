51be282fc0e2843b434341253052f90d46222cfb
YunK-ei <1002578377@qq.com>
2024-01-14 16:42:43 +0800

    before starting pa2

--- npc/Digital_Lab/test/vsrc/ctrl.v
+++ npc/Digital_Lab/test/vsrc/ctrl.v
@@ -1,22 +1,9 @@
-module ctrl
-	(
-	input clk,
-	input [3:0] limi
-	);
-	reg [3:0] counter;
-	
-	initial begin
-		assign counter = limi;
-	end
-	
+module ctrl(
+    input sig_a,
+    input sig_b,
+    output sig_out
+);
 
-	always @(posedge clk)begin
-		if(counter > 0)begin
-			counter <= counter - 1;
-		end
-		else begin
-			$write("*-* All Finished *-*\n");
-			$finish;
-		end
-	end
-	endmodule
+assign sig_out = sig_a && sig_b;
+
+endmodule
