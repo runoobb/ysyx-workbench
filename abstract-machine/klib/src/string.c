0cc823a8fbe53d276fa4b0db11ea3906b2907aef
YunK-ei <1002578377@qq.com>
2024-04-11 23:44:20 +0800

    complete the isa decoder && string klib

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,23 +5,61 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t tmp = 0;
+  while(*s)
+  {
+    s++;
+    tmp++;
+  }
+  return tmp;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+        while(*src)
+        {
+          *dst = *src;
+          dst++;
+          src++;
+        }
+        *dst = 0;
+        return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  for(int i = 0; i < n; i++)
+  {
+    if(i >= strlen(src))
+      *(dst+i) = 0;
+    else
+      *(dst+i) = *(src+i);
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char* head = dst + strlen(dst);
+  while(*src)
+  {
+    *head = *src;
+    src++;
+    head++;
+  }
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int ret = 0;
+  while(!(ret=*(unsigned char*)s1 - *(unsigned char*)s2) && *s1)
+  {
+    s1++;
+    s2++;
+  }
+  if(ret < 0)
+          return -1;
+  else if(ret > 0)
+          return 1;
+  else
+          return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +67,15 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+        unsigned char uc = c;
+        unsigned char *su;
+        for(su = s; n > 0; n--, su++)
+        {
+          *su = uc;
+        }
+
+        return s;
+
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +87,14 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  int ret = 0;
+  while(n && !(ret = (*(unsigned char *)s1) - (*(unsigned char *)s2)))
+  {
+    s1++;
+    s2++;
+    n--;
+  }
+  return ret;
 }
 
 #endif
