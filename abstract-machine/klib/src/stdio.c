506cf80ab28814ba919c150f3a6a7ee65f928d64
YunK-ei <1002578377@qq.com>
2024-06-26 01:52:26 +0800

    add more format to sprintf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,31 +5,76 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int my_itoa(int num, char *str, int base, bool leading_zeros, bool padding, int width) {
+  char *digits = "0123456789ABCDEF";
+  int ilen = 0;
+  bool is_negative = false;
+
+  if (num < 0) {
+    is_negative = true;
+    num = -num;
+  }
+
+  do {
+    str[ilen++] = digits[num % base];
+    num /= base;
+  } while (num > 0);
+
+  if (is_negative) {
+    str[ilen++] = '-';
+  }
+
+  // Handle leading zeros
+  if (leading_zeros) {
+    int zeros = width - ilen;
+    while (zeros-- > 0) {
+      str[ilen++] = '0';
+    }
+  }
+
+  // Pad with spaces
+  if (padding) {
+    int spaces = width - ilen;
+    while (spaces-- > 0) {
+      str[ilen++] = ' ';
+    }
+  }
+
+  // str[i] = '\0';
+
+  // Reverse the string
+  for (int j = 0; j < ilen / 2; j++) {
+    char temp = str[j];
+    str[j] = str[ilen - j - 1];
+    str[ilen - j - 1] = temp;
+  }
+
+  return ilen;
+}
+
 int printf(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
   char *outstr = malloc(64);
-  int ret =  sprintf(outstr, fmt);
+  int ret =  vsprintf(outstr, fmt, args);
   putstr(outstr);
+  free(outstr);
+  va_end(args);
   return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
   int d;
-  int d_cpy;
-  int bit;
   int length = 0;
-  char *s ;
-  va_start(ap, fmt);
+  int delta_length;
+  int nested_num;
+  char *s;
   while (*fmt)
   {
           if(*fmt == '%')
           {
             fmt++;
-            switch (*(fmt++))
+            switch (*fmt)
             {
               case 's':
                       s = va_arg(ap, char *);
@@ -44,25 +89,139 @@ int sprintf(char *out, const char *fmt, ...) {
                       //*out = 0;
                       break;
               case 'd':
-                      bit = 0;
                       d = va_arg(ap, int);
-                      d_cpy = d;
-                      do
-                      {
-                              d = d/10;
-                              bit += 1;
-                      }while(d);
+                      delta_length = my_itoa(d, out, 10, false, false, 0);
+                      out = out + delta_length;
+                      length += delta_length;
+                      break;
+              case 'x':
+                      d = va_arg(ap, int);
+                      delta_length = my_itoa(d, out, 16, false, false, 0);
+                      out = out + delta_length;
+                      length += delta_length;
+                      break;
+              case '0':
+                fmt++;
+                switch(*fmt)
+                {
+                  case '1':
+                  case '2':
+                  case '3':
+                  case '4':
+                  case '5':
+                  case '6':
+                  case '7':
+                  case '8':
+                    nested_num = atoi((char *)fmt);
+                    fmt++;
+                    switch(*fmt)
+                    {
+                      case 'd':
+                              d = va_arg(ap, int);
+                              delta_length = my_itoa(d, out, 10, true, false, nested_num);
+                              out = out + delta_length;
+                              length += delta_length;
+                              break;
                       
-                      for(int i = bit-1; i >= 0; i--)
+                      case 'x':
+                              d = va_arg(ap, int);
+                              delta_length = my_itoa(d, out, 16, true, false, nested_num);
+                              out = out + delta_length;
+                              length += delta_length;
+                              break;                      
+                    }
+                    break;
+                }            
+            }
+            fmt++;
+          }
+          else
+          {
+                  *out = *fmt;
+                  //putch(*out);
+                  fmt++;
+                  out++;
+                  length++;
+          }
+            
+  }
+  *out = 0;
+  return length;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  int d;
+  int length = 0;
+  int delta_length;
+  int nested_num;
+  char *s;
+  va_start(ap, fmt);
+  while (*fmt)
+  {
+          if(*fmt == '%')
+          {
+            fmt++;
+            switch (*fmt)
+            {
+              case 's':
+                      s = va_arg(ap, char *);
+                      while(*s)
                       {
-                              *(out+i) = (d_cpy % 10 + 48);
-                              //putch(*(out+i));
-                              d_cpy = d_cpy/10;
+                              *out = *s;
+                              //putch(*out);
+                              out++;
+                              s++;
                               length++;
                       }
-                      out = out + bit;
+                      //*out = 0;
                       break;
+              case 'd':
+                      d = va_arg(ap, int);
+                      delta_length = my_itoa(d, out, 10, false, false, 0);
+                      out = out + delta_length;
+                      length += delta_length;
+                      break;
+              case 'x':
+                      d = va_arg(ap, int);
+                      delta_length = my_itoa(d, out, 16, false, false, 0);
+                      out = out + delta_length;
+                      length += delta_length;
+                      break;
+              case '0':
+                fmt++;
+                switch(*fmt)
+                {
+                  case '1':
+                  case '2':
+                  case '3':
+                  case '4':
+                  case '5':
+                  case '6':
+                  case '7':
+                  case '8':
+                    nested_num = atoi((char *)fmt);
+                    fmt++;
+                    switch(*fmt)
+                    {
+                      case 'd':
+                              d = va_arg(ap, int);
+                              delta_length = my_itoa(d, out, 10, true, false, nested_num);
+                              out = out + delta_length;
+                              length += delta_length;
+                              break;
+                      
+                      case 'x':
+                              d = va_arg(ap, int);
+                              delta_length = my_itoa(d, out, 16, true, false, nested_num);
+                              out = out + delta_length;
+                              length += delta_length;
+                              break;                      
+                    }
+                    break;
+                }                                 
             }
+            fmt++;
           }
           else
           {
