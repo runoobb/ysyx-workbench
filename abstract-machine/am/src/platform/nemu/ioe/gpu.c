319ef2b89f924aab592b5b17743564c677f39350
YunK-ei <1002578377@qq.com>
2024-07-01 11:05:40 +0800

    fix vga && run mario

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,18 +3,32 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+//static picture display, windows size = 400 * 300 in VM
+//fb stores the picture
 void __am_gpu_init() {
+  // int i;
+  // int w = 400;  // TODO: get the correct width
+  // int h = 300;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = 400, .height = 300,
+// adjust .width & .height
+// .width = 32 * 1, .height = 32 * 1,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for(int i = 0; i < ctl->h; i++)
+    for(int j = 0; j < ctl->w; j++)
+      fb[(ctl->y + i) * 400 + ctl->x + j] = *((uint32_t *)(ctl->pixels) + i * ctl->w + j); 
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,4 +6,10 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
+  uint32_t tmp = inl(KBD_ADDR);
+  kbd->keydown = (tmp >> 15 == 1);
+  if(!kbd->keydown)
+    kbd->keycode = tmp;
+  else
+    kbd->keycode = tmp & ~KEYDOWN_MASK; 
 }
