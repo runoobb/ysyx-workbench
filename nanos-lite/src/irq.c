3d92b34f2e858c7b1f362e33e8a9971d42f2652c
YunK-ei <1002578377@qq.com>
2024-10-17 20:44:13 +0800

    _sbrk() finish, but how malloc() & printf() & sbrk() invoked

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      yield();
+      // pass by
       break;
     case EVENT_SYSCALL:
       do_syscall(c);
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,29 +1,6 @@
 #include <common.h>
 #include "syscall.h"
 
-enum {
-  SYS_exit,
-  SYS_yield,
-  SYS_open,
-  SYS_read,
-  SYS_write,
-  SYS_kill,
-  SYS_getpid,
-  SYS_close,
-  SYS_lseek,
-  SYS_brk,
-  SYS_fstat,
-  SYS_time,
-  SYS_signal,
-  SYS_execve,
-  SYS_fork,
-  SYS_link,
-  SYS_unlink,
-  SYS_wait,
-  SYS_times,
-  SYS_gettimeofday
-};
-
 //no ecall in syscall handler
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -33,24 +10,23 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
   switch (a[0]) {
     case SYS_exit:
-      printf("Exit:a[1], a[2], a[3] = %d, %d, %d\n", a[1], a[2], a[3]);
+      Log("Exit:a[1], a[2], a[3] = %d, %d, %d\n", a[1], a[2], a[3]);
       if(a[1] == 0)
         halt(0);
       else
-        halt(-1);
+        halt(1);
       break;
     case SYS_yield:
-      // printf("%d, %d, %d, %d\n", a[0], a[1], a[2], a[3]);
-      // c->GPRx = -1;
-      // printf("%d\n", c->GPR2);
       yield();
       c->GPRx = 0;
       break;
+    // @TODO
     case SYS_brk:
-      printf("Brk:a[1], a[2], a[3] = %x, %x, %x\n", a[1], a[2], a[3]);
+      Log("Brk:a[1], a[2], a[3] = %x, %x, %x\n", a[1], a[2], a[3]);
       c->GPRx = 0;
       break;
     case SYS_write:
+      Log("Write:a[1], a[2], a[3] = %x, %x, %x\n", a[1], a[2], a[3]);
       if(a[1] == 1 || a[1] == 2)
       {
         for(int i = 0; i < a[3]; i++)
